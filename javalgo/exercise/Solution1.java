b06b4589f7f717177ad9f9d4eaa90628e2604c83
jjungeun <jungeun9729@gmail.com>
2019-10-09 21:06:50 +0900

    완주하지 못한 선수

---
+++ javalgo/exercise/Solution1.java
@@ -0,0 +1,33 @@
+package javalgo.exercise;
+
+import java.util.HashMap;
+
+// 완주하지 못한 선수
+public class Solution1 {
+  public static void main(String[] args) {
+    String[] participant = { "leo", "kiki", "eden" };
+    String[] completion = { "eden", "kiki" };
+    Solution1 sol = new Solution1();
+    System.out.println(sol.solution(participant, completion));
+  }
+
+  public String solution(String[] participant, String[] completion) {
+    String answer = "";
+    HashMap<String, Integer> map = new HashMap<>();
+
+    for (String p : participant) {
+      map.put(p, map.getOrDefault(p, 0) + 1);
+    }
+    for (String c : completion) {
+      map.put(c, map.get(c) - 1);
+    }
+
+    for (String key : map.keySet()) {
+      if (map.get(key) != 0) {
+        answer = key;
+        break;
+      }
+    }
+    return answer;
+  }
+}
