6f322b290c30b47e89c195e8dcabb2c680401f49
jjungeun <jungeun9729@gmail.com>
2019-10-22 19:31:04 +0900

    서울에서 김서방 찾기

--- javalgo/exercise/Solution16.java
+++ javalgo/exercise/Solution16.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution16 {
   public static void main(String[] args) {
     String[] seoul = { "Jane", "Kim" };
@@ -8,10 +10,19 @@ class Solution16 {
   public String solution(String[] seoul) {
     int answer = 0;
     for (int i = 0; i < seoul.length; i++) {
-      if (seoul[i] == "Kim") {
+      if (seoul[i].equals("Kim")) {
         answer = i;
+        break;
       }
     }
     return "김서방은 " + Integer.toString(answer) + "에 있다";
+
+    // ----------------------------------------------------
+    // best solution
+    // Arrays.asList()는 배열을 list처럼 사용하는 방법이다. 
+    // 원소 추가나 삭제는 못하지만 이 문제처럼 탐색하는 경우 유용하다.
+    // ----------------------------------------------------
+    // int answer = Arrays.asList(seoul).indexOf("Kim");
+    // return "김서방은 " + answer + "에 있다";
   }
 }
