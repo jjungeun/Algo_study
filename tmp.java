86a311c7c7142b67d596658d445a615e8f3562f1
jjungeun <jungeun9729@gmail.com>
2020-04-23 23:09:55 +0900

    ex2

--- tmp.java
+++ tmp.java
@@ -1,27 +1,22 @@
+import java.util.Arrays;
+
 class tmp {
 	public static void main(String[] argv) {
-		String s = "cde";
-		System.out.println(new tmp().solution(s));
+		String s1 = "cde";
+		String s2 = "efd";
+		System.out.println(new tmp().solution(s1, s2));
 	}
 
-	// boolean solution(String s) {
-	// boolean[] check = new boolean[256];
-	// for (int i = 0; i < s.length(); i++) {
-	// if (check[s.charAt(i)])
-	// return false;
-	// check[s.charAt(i)] = true;
-	// }
-	// return true;
-	// }
-	boolean solution(String s) {
-		int checker = 0;
-		for (int i = 0; i < s.length(); i++) {
-			int val = s.charAt(i) - 'a';
-			System.out.println(checker);
-			if ((checker & (1 << val)) > 0)
-				return false;
-			checker |= (1 << val);
-		}
-		return true;
+	int[] get_numChar(String s){
+		int[] numChar = new int[128];
+		for(int i = 0; i < s.length(); i++)
+			numChar[s.charAt(i)] += 1;
+		return numChar;
+	}
+
+	boolean solution(String s1, String s2) {
+		if (s1.length() != s2.length())
+			return false;
+		return Arrays.equals(get_numChar(s1),get_numChar(s2));
 	}
 }
