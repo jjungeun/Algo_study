01357e469d9b7c7a9613f8f70348a62151a3b5ea
jjungeun <jungeun9729@gmail.com>
2020-04-24 14:59:11 +0900

    숫자의 표현

---
+++ javalgo/exercise/Solution82.java
@@ -0,0 +1,25 @@
+
+public class Solution82 {
+	public static void main(String[] args){
+		int n = 15;
+		System.out.println(new Solution82().solution(n));
+	}
+
+	public int solution(int n) {
+		int answer = 1;
+		for(int i = 1; i <= n / 2; i++)
+		{
+			int sum = i;
+			for(int j = i + 1; sum < n; j++)
+			{
+				sum += j;
+				if (sum == n)
+				{
+					++answer;
+					break ;
+				}
+			}
+		}
+		return answer;
+	}
+}
