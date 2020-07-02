129753ca91641c5c6c1655781b60e165c1514088
Jungeun Shin <jushin@c2r9s1.42seoul.kr>
2020-07-02 15:52:07 +0900

    89

---
+++ javalgo/exercise/Solution89.java
@@ -0,0 +1,13 @@
+public class Solution89 {
+	public static void main(String[] args)
+	{
+		String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+		int[] plays = {500, 600, 150, 800, 2500};
+		System.out.println(new Solution89().solution(genres, plays);
+	}
+
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
