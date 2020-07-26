31fbc566824c8dc039e32b9e6bced3625a206930
jjungeun <jungeun9729@gmail.com>
2020-07-26 22:49:06 +0900

    징검다리 건너기

--- python/kakao/Solution14.py
+++ python/kakao/Solution14.py
@@ -1,49 +1,45 @@
-def solution(stones, k):
-	answer = 200000000
-	idx = -1
-	if k == 1:
-		return min(stones)
-	while idx + k <= len(stones):
-		if idx + k < len(stones):
-			next_stone = stones[idx+1:idx+k+1]
-			next_max = max(next_stone)
-			if next_max < answer:
-				answer = next_max
-			idx += next_stone.index(next_max) + 1
+def is_possible(stones, value, k):
+	for i in range(len(stones)):
+		stones[i] = 1 if stones[i] > value else 0
+	zeros = 0
+	for s in stones:
+		if s == 0:
+			zeros += 1
+			if zeros == k:
+				return False
 		else:
-			break
-	return answer
+			zeros = 0
+	return True
 
-stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
-# stones = [2, 44, 5]
+def solution(stones, k):
+	min_value = min(stones)
+	max_value = max(stones)
+	while min_value <= max_value:
+		value = int((max_value + min_value) / 2)
+		if is_possible(stones[:], value, k):
+			min_value = value + 1
+		else:
+			max_value = value - 1
+	return min_value
+
+# stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
+stones = [2, 44, 5]
 
 k = 3
 print(solution(stones, k))
 
-# def go_possible(check, k):
-# 	never_go = 0
-# 	for c in check:
-# 		if c == 0:
-# 			never_go += 1
-# 			if never_go >= k:
-# 				return False
-# 		else:
-# 			never_go = 0
-# 	return True
-
 # def solution(stones, k):
-# 	answer = 1
-# 	stone_case = {}
-# 	for i, v in enumerate(stones):
-# 		if v in stone_case.keys():
-# 			stone_case[v] += [i]
+# 	answer = 200000000
+# 	idx = -1
+# 	if k == 1:
+# 		return min(stones)
+# 	while idx + k <= len(stones):
+# 		if idx + k < len(stones):
+# 			next_stone = stones[idx+1:idx+k+1]
+# 			next_max = max(next_stone)
+# 			if next_max < answer:
+# 				answer = next_max
+# 			idx += next_stone.index(next_max) + 1
 # 		else:
-# 			stone_case[v] = [i]
-# 	check = [0 for i in range(len(stones))]
-# 	for case in sorted(stone_case.keys(), reverse=True):
-# 		for idx in stone_case[case]:
-# 			check[idx] = 1
-# 		if go_possible(check, k):
-# 			answer = case
 # 			break
 # 	return answer
