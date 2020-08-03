af0d81f0f16afb0afd0893c24196d83d4b5dbcd1
jjungeun <jungeun9729@gmail.com>
2020-08-03 22:19:06 +0900

    매칭점수

---
+++ python/kakao/Solution20.py
@@ -0,0 +1,16 @@
+import re
+
+def solution(word, pages):
+    meta_parser = re.compile('<meta(.+?)/>')
+    a_parser = re.compile('<a href="(.+?)">')
+    page_infos = []
+    for page in pages:
+        page_dict = {}
+        a_tags = a_parser.findall(page)
+        meta_tag = meta_parser.search(page).group().split(' ')[2]
+        page_dict['url'] = meta_tag.split('"')[1]
+        page_dict['outer_list'] = a_tags
+
+word = "blind"
+pages = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
+print(solution(word, pages))
