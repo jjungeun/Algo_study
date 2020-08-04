6609d06d9dd42abdd7b899475cfa60f16a4dbbc1
jjungeun <jungeun9729@gmail.com>
2020-08-04 20:17:29 +0900

    자물쇠와 열쇠

--- python/kakao/Solution4.py
+++ python/kakao/Solution4.py
@@ -1,105 +1,44 @@
 import copy
+def init_lock(lock, M, N):
+	new_lock = [[0 for _ in range(N+2*M-2)] for _ in range(N+2*M-2)]
+	for row_idx in range(N):
+		for col_idx in range(N):
+			new_lock[row_idx+M-1][col_idx+M-1] = lock[row_idx][col_idx]
+	return new_lock
 
-def turn(key):
-	newkey = []
-	N = len(key)
-	for rid in range(N):
-		row = len(key) - rid - 1
-		for col in range(N):
-			if len(newkey) <= col:
-				newkey.append([key[row][col]])
-			else:
-				newkey[col].append(key[row][col])
-	return newkey
+def turn_key(key, M):
+	new_key = []
+	for col_idx in range(M):
+		new_row = []
+		for row_idx in range(M):
+			new_row.append(key[M-row_idx-1][col_idx])
+		new_key.append(new_row)
+	return new_key
 
-def ismatch(key, lock, rdiff, cdiff):
-	N = len(key)
-	M = len(lock)
-	tmp_lock = copy.deepcopy(lock)
-	for r in range(N):
-		for c in range(N):
-			if key[r][c] == 1 and r + rdiff >= 0 and r + rdiff < M and c + cdiff >= 0 and c + cdiff < M:
-				tmp_lock[r+rdiff][c+cdiff] += 1
-	for r in range(M):
-		for c in range(M):
-			if tmp_lock[r][c] != 1:
+def is_key(result, M, N):
+	for row_idx in range(M-1, M+N-1):
+		for col_idx in range(M-1, M+N-1):
+			if result[row_idx][col_idx] != 1:
 				return False
 	return True
 
-def iskey(key, lock, kr, kc):
-	M = len(lock)
-	for r in range(M):
-		for c in range(M):
-			if lock[r][c] == 0:
-				rdiff = r - kr
-				cdiff = c - kc
-				if ismatch(key, lock, rdiff, cdiff):
-					return True
+def test_all_case(key, lock, M, N):
+	for row_idx in range(M+N-1):
+		for col_idx in range(M+N-1):
+			result = copy.deepcopy(lock)
+			for r in range(M):
+				for c in range(M):
+					result[row_idx + r][col_idx + c] += key[r][c]
+			if is_key(result, M, N):
+				return True
 	return False
 
 def solution(key, lock):
-	N = len(key)
-	for i in range(0, 4):
-		for kr in range(N):
-			for kc in range(N):
-				if key[kr][kc] == 1 and iskey(key, lock, kr, kc):
-					return True
-		key = turn(key)
+	M = len(key)
+	N = len(lock)
+	lock = init_lock(lock, M, N)
+	for _ in range(4):
+		if test_all_case(key, lock, M, N):
+			return True
+		key = turn_key(key, M)
 	return False
-
-key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-solution(key, lock)
-
-# lock
-# 1 1 1
-# 1 1 0
-# 1 0 1
-
-
-# 1,2
-# 1,0
-# rdiff = 2
-# cdiff = 0
-
-# 0 0 0
-# 1 0 0
-# 0 1 1
-
-# 0 1 0
-# 1 0 0
-# 1 0 0
-
-# 1 1 0
-# 0 0 1
-# 0 0 0
-
-# 0 0 1
-# 0 0 1
-# 0 1 0
-
-##########33
-
-# 지금
-# 1 1 1
-# 2 1 0
-# 1 1 2
-
-# 90
-# 1 2 1
-# 2 1 0
-# 2 0 1
-
-# 180
-# 2 2 1
-# 1 1 1
-# 1 0 1
-
-# 270
-# 1 1 2
-# 1 1 1
-# 1 1 1
-
-# 조건
-# - 0의 개수가 key의 개수보다 크면 false
-# -
