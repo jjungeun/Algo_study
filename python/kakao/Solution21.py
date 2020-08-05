61916e816062f54d6a1aed523844934da9d12829
jjungeun <jungeun9729@gmail.com>
2020-08-05 21:16:51 +0900

    로봇청소기

---
+++ python/kakao/Solution21.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+
+turn_dr = [-1, 0,1,0]
+turn_dc = [0, 1,0,-1]
+
+back_dr = [1,0,-1,0]
+back_dc = [0,-1,0,1]
+
+def solution(map_info, pos_info, area):
+	answer = 1
+	r, c, d = pos_info
+	area[r][c] = 2
+	q = deque([[r, c, d]])
+	N, M = map_info
+	while q:
+		r, c, d = q.popleft()
+		tmp_d = d
+		for i in range(4):
+			tmp_d = (tmp_d + 3) % 4
+			new_r, new_c = r + turn_dr[tmp_d], c + turn_dc[tmp_d]
+
+			# a
+			if new_r in range(0, N) and new_c in range(0, M) and area[new_r][new_c] == 0:
+				area[new_r][new_c] = 2
+				answer += 1
+				q.append([new_r, new_c, tmp_d])
+				break
+			# c
+			if i == 3:
+				new_r, new_c = r + back_dr[d], c + back_dc[d]
+				q.append([new_r, new_c, d])
+				if area[new_r][new_c] == 1:
+					return answer
+
+if __name__ == "__main__":
+	map_info = map(int, sys.stdin.readline().rstrip().split(' '))
+	pos_info = map(int, sys.stdin.readline().rstrip().split(' '))
+	area = []
+	for row_idx in range(map_info[0]):
+		area.append(list(map(int, sys.stdin.readline().rstrip().split(' '))))
+	print(solution(map_info, pos_info, area))
