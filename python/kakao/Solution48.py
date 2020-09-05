8620bf96cbc2f5d680a02f7340755d82e932cfe3
jjungeun <jungeun9729@gmail.com>
2020-09-05 17:42:49 +0900

    해킹(다익스트라)

--- python/kakao/Solution48.py
+++ python/kakao/Solution48.py
@@ -1,35 +1,39 @@
-def next_node(n, relation, visited):
-	min_value = 1001
-	node = -1
-	for i in range(n):
-		if not visited[i] and relation[i] < min_value:
-			node = i
-	return node+1
+import sys
+from heapq import heappush, heappop
+INF = float('inf')
 
-def search(n, c, coms):
-	infected, max_cost = 0, 0
-	relation = [1001 for _ in range(n)]
-	visited = [False for _ in range(n)]
-	relation[c-1] = 0
-	while True:
-		visited[c-1] = True
-		infected += 1
-		for com in coms:
-			c1, c2, cost = com
-			if c2 == c:
-				relation[c1-1] = min(relation[c1-1], relation[c-1]+cost)
-		c = next_node(n, relation, visited)
-		if c == 0:
-			break
-	for cost in relation:
-		if cost != 1001:
-			max_cost = max(max_cost, cost)
-	print(infected, max_cost)
+def hacking(relation, dist, start):
+	heap = []
+	dist[start] = 0
+	heappush(heap, [dist[start], start])
+	while heap:
+		cur_dist, cur_idx = heappop(heap)
+		if cur_idx not in relation.keys():
+			continue
+		for k, v in relation[cur_idx].items():
+			distance = cur_dist + v
+			if dist[k] > distance:
+				dist[k] = distance
+				heappush(heap, [dist[k], k])
+	return dist
 
 N = int(input())
 for _ in range(N):
-	n, d, c = list(map(int, input().split(' ')))
-	coms = []
-	for _ in range(d):
-		coms.append(list(map(int, input().split(' '))))
-	search(n, c, coms)
+	V, tc, c = map(int, sys.stdin.readline().split())
+	relation = {}
+	dist = [INF for _ in range(V+1)]
+	for _ in range(tc):
+		end, start, distance = map(int, sys.stdin.readline().split())
+		if start in relation.keys():
+			relation[start][end] = distance
+		else:
+			relation[start] = {end: distance}
+	dist = hacking(relation, dist, c)
+
+	infected = 0
+	final_infect = 0
+	for d in dist:
+		if d != INF:
+			infected += 1
+			final_infect = max(final_infect, d)
+	print(infected, final_infect)
