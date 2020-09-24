6b7f485f1836e23c8da67db609e798b09788d4dd
jjungeun <jungeun9729@gmail.com>
2020-09-24 16:55:43 +0900

    스타트택시(bfs)

---
+++ python/kakao/Solution61.py
@@ -0,0 +1,93 @@
+from collections import deque
+
+dr = [-1,  0, 0, 1]
+dc = [0, -1, 1, 0]
+
+def get_dist(taxi_map, start_pos, end_pos):
+	sr, sc = start_pos
+	er, ec = end_pos
+	dist = -1
+	visited = [[False for _ in range(N)] for _ in range(N)]
+	visited[sr][sc] = True
+	q = deque()
+	q.append((sr, sc, 0))
+	while q:
+		r, c, d = q.popleft()
+		if (r, c) == (er, ec):
+			dist = d
+			break
+		for i in range(4):
+			nr, nc = r+dr[i], c+dc[i]
+			if nr not in range(0, N) or nc not in range(0, N):
+				continue
+			if taxi_map[nr][nc] == 0 and not visited[nr][nc]:
+				q.append((nr, nc, d+1))
+				visited[nr][nc] = True
+	return dist
+
+def find_guest(taxi_map, taxi_pos, guests):
+	tr, tc = taxi_pos
+	min_dist = N * N
+	min_idx = -1
+	mr, mc = N, N
+	visited = [[False for _ in range(N)] for _ in range(N)]
+	visited[tr][tc] = True
+	q = deque()
+	q.append((tr, tc, 0))
+	while q:
+		r, c, d = q.popleft()
+		if d > min_dist:
+			continue
+		if guests_pos[r][c] != -1 and (r < mr or (r == mr and c < mc)):
+			min_idx = guests_pos[r][c]
+			min_dist = d
+			mr, mc = r, c
+			continue
+		for i in range(4):
+			nr, nc = r+dr[i], c+dc[i]
+			if nr not in range(0, N) or nc not in range(0, N):
+				continue
+			if taxi_map[nr][nc] == 0 and not visited[nr][nc]:
+				q.append((nr, nc, d+1))
+				visited[nr][nc] = True
+	if mr in range(0, N) and mc in range(0, N):
+		guests_pos[mr][mc] = -1
+	return min_idx, min_dist
+
+def do_work(taxi_map, fuel, taxi_pos, guests):
+	answer = fuel
+	cnt = 0
+	while cnt < len(guests):
+		idx, to_guest_dist = find_guest(taxi_map, taxi_pos, guests)
+		if idx == -1:
+			answer = -1
+			break
+		answer -= to_guest_dist
+		start, end = guests[idx]
+		to_end_dist = get_dist(taxi_map, start, end)
+		answer -= to_end_dist
+		if answer < 0 or to_end_dist == -1:
+			answer = -1
+			break
+		taxi_pos = end
+		answer += to_end_dist * 2
+		cnt += 1
+	return answer
+
+N, M, fuel = map(int, input().split(' '))
+taxi_map = []
+for _ in range(N):
+	taxi_map.append(list(map(int, input().split(' '))))
+taxi_pos = map(int, input().split(' '))
+taxi_pos = [pos-1 for pos in taxi_pos]
+guests = []
+for _ in range(M):
+	sr, sc, er, ec = map(int, input().split(' '))
+	guests.append([[sr-1, sc-1], [er-1, ec-1]])
+guests = sorted(guests)
+guests_pos = [[-1 for _ in range(N)] for _ in range(N)]
+for idx, guest in enumerate(guests):
+	start, end = guest
+	sr, sc = start
+	guests_pos[sr][sc] = idx
+print(do_work(taxi_map, fuel, taxi_pos, guests))
